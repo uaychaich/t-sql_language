c679f95738f7cf3fef0ccb40bca4273ee3019453
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-01-30 22:11:18 +0700

    Added 202601302211

--- 14b_c4bb_Modify_Data.sql
+++ 14b_c4bb_Modify_Data.sql
@@ -45,3 +45,62 @@ JOIN uay.Register r ON nl.LoginID = r.LoginID
 WHERE r.ExibitionName = 'Exhibition1';
 
 -------------------------
+
+MERGE uay.NewLogin AS target
+USING (SELECT LoginID, LoginName FROM uay.OldLogin) AS source
+ON target.LoginID = source.LoginID
+WHEN MATCHED THEN
+    UPDATE SET 
+        target.LoginName = source.LoginName,
+        target.ModifiedDate = GETDATE()
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (LoginID, LoginName, ModifiedDate)
+    VALUES (source.LoginID, source.LoginName, GETDATE())
+WHEN NOT MATCHED BY SOURCE THEN
+    DELETE;
+
+SELECT * FROM uay.NewLogin;
+
+-------------------------
+
+INSERT INTO uay.NewLogin (LoginID, LoginName) OUTPUT inserted.* 
+VALUES (70, 'User7'), (80, 'User8');
+
+UPDATE uay.NewLogin SET LoginName = LoginName + FORMAT(GETDATE(), 'yyyyMMddhhmmss') OUTPUT inserted.*, deleted.*
+WHERE LoginID IN (70, 80);
+
+DELETE FROM uay.NewLogin OUTPUT deleted.* WHERE LoginID IN (70, 80);
+
+MERGE uay.NewLogin AS target
+USING (SELECT LoginID, LoginName FROM uay.OldLogin) AS source
+ON target.LoginID = source.LoginID
+WHEN MATCHED THEN
+    UPDATE SET 
+        target.LoginName = source.LoginName,
+        target.ModifiedDate = GETDATE()
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (LoginID, LoginName, ModifiedDate)
+    VALUES (source.LoginID, source.LoginName, GETDATE())
+WHEN NOT MATCHED BY SOURCE THEN
+    DELETE
+OUTPUT $action, inserted.*, deleted.*;
+
+-------------------------
+
+SELECT * INTO #TempLogin FROM uay.NewLogin;
+
+SELECT * FROM #TempLogin;
+
+-------------------------
+
+DELETE #TempLogin;
+
+DELETE FROM uay.NewLogin WHERE LoginID IN (70,80);
+
+INSERT INTO uay.NewLogin (LoginID, LoginName) OUTPUT inserted.* INTO #TempLogin
+VALUES (70, 'User7'), (80, 'User8');
+
+SELECT * FROM uay.NewLogin;
+SELECT * FROM #TempLogin;
+
+-------------------------
---
+++ 15_e23b_Programming_Variable.sql
@@ -0,0 +1,35 @@
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+
+---------------------------------------
+
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+
+---------------------------------------
+
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+
+---------------------------------------
+
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+
+---------------------------------------
+
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
+DECLARE @data01 INT;SET @data01 = 5;SELECT @data01 AS Result;
